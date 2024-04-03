874b05a5781cdfecb781217f1df1b61b5f248323
Kaio C. de Oliveira <hello@kaio.dev>
2024-04-03 11:19:51 -0300

    refactor: update data result doc strings and class modifiers

--- lib/src/core/data_result/data_result.dart
+++ lib/src/core/data_result/data_result.dart
@@ -1,52 +1,75 @@
+/// This follows the Railway Oriented Programming (ROP) pattern.
+///
+/// The `DataResult` class represents the result of an operation that can either be a success or a failure.
+///
+/// If the operation is successful, the `DataResult` will contain the data of type `S`. Otherwise, it will contain the failure of type `F`.
+///
+/// For example:
+///
+/// ```dart
+/// final result = DataResult.success(42);
+///
+/// result.fold(
+///  (data) => print('Data: $data'), // prints 'Data: 42'
+///  (failure) => print('Failure: $failure'),
+/// );
+/// ```
 sealed class DataResult<S, F> {
   const DataResult._internal();
 
-  factory DataResult.success(S data) = _Success;
+  /// Creates a success result with the given data.
+  factory DataResult.success(S data) = SuccessData;
 
-  factory DataResult.failure(F failure) = _Failure;
+  /// Creates a failure result with the given failure.
+  factory DataResult.failure(F failure) = FailureData;
 
-  bool get isSuccess => this is _Success<S, F>;
+  /// Returns `true` if the operation was successful.
+  bool get isSuccess => this is SuccessData<S, F>;
 
-  bool get isFailure => this is _Failure<S, F>;
+  /// Returns `true` if the operation was not successful.
+  bool get isFailure => this is FailureData<S, F>;
 
+  /// Returns the data if the operation was successful.
   S? get data => fold<S?>(
         (data) => data,
         (failure) => null,
       );
 
+  /// Returns the failure if the operation was not successful.
   F? get failure => fold<F?>(
         (data) => null,
         (failure) => failure,
       );
 
+  /// Returns the result of the operation based on whether it was successful or not.
   R fold<R>(
     R Function(S data) onSuccess,
     R Function(F failure) onFailure,
   );
 }
 
-class _Success<S, F> extends DataResult<S, F> {
-  _Success(this._value) : super._internal();
+final class SuccessData<S, F> extends DataResult<S, F> {
+  SuccessData(this.value) : super._internal();
 
-  final S _value;
+  final S value;
 
   @override
   R fold<R>(
     R Function(S data) onSuccess,
     R Function(F failure) onFailure,
   ) =>
-      onSuccess(_value);
+      onSuccess(value);
 }
 
-class _Failure<S, F> extends DataResult<S, F> {
-  _Failure(this._value) : super._internal();
+final class FailureData<S, F> extends DataResult<S, F> {
+  FailureData(this.value) : super._internal();
 
-  final F _value;
+  final F value;
 
   @override
   R fold<R>(
     R Function(S data) onSuccess,
     R Function(F failure) onFailure,
   ) =>
-      onFailure(_value);
+      onFailure(value);
 }
